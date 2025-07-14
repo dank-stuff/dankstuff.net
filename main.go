be3ab13f00dd995a1310f63f3a8d685e210c7ac3
Baraa Al-Masri <pub@mbaraa.com>
2025-07-14 13:06:47 +0300

    Improve dank products listing!

--- main.go
+++ main.go
@@ -23,22 +23,24 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_ = t.Execute(w, map[string]any{
 			"BG": fmt.Sprintf("/assets/bgs/%d.webp", rand.Intn(5)+1),
-			"Deployments": []struct {
+			"DankStuffProducts": []struct {
 				Title       string
 				Link        string
 				Description string
+				LogoPath    string
+				WIP         bool
 			}{
 
-				{Title: "DankMuzikk", Link: "https://dankmuzikk.com", Description: "Create, Share and Play Music Playlists."},
-				{Title: "DankLyrics", Link: "https://danklyrics.com", Description: "Find lyrics for songs or something."},
-				{Title: "DankScreen", Link: "https://screen.dankstuff.net", Description: "Display capture card's output into your browser."},
-				{Title: "DankTodo", Link: "https://todo.dankstuff.net", Description: "The first htmx app with C (Ulfius)"},
-				{Title: "DankNotes", Link: "https://notes.dankstuff.net", Description: "Bootleg version of Notion and Google Notes. (WIP)"},
-				// {Title: "DankSim", Link: "https://danksim.com", Description: "Get an eSIM qucickly, ANYWHERE."},
-				{Title: "DankDysk", Link: "https://dankdysk.com", Description: "Magical unlimited cloud drive. (WIP)"},
-				{Title: "DankTorrent", Link: "https://dankdysk.com", Description: "Download anything as torrent. (WIP)"},
-				{Title: "DankIP", Link: "https://dankip.com", Description: "Get a public IP for any server/computer. (WIP)"},
-				{Title: "libdank", Link: "https://libdank.org", Description: "File compression extension but the result is a video. (WIP)"},
+				{Title: "DankMuzikk", Link: "https://dankmuzikk.com", Description: "Create, Share and Play Music Playlists.", LogoPath: "/assets/logos/dankmuzikk.webp"},
+				{Title: "DankLyrics", Link: "https://danklyrics.com", Description: "Find lyrics for songs or something.", LogoPath: "/assets/logos/danklyrics.png"},
+				{Title: "DankScreen", Link: "https://screen.dankstuff.net", Description: "Display capture card's output into your browser.", LogoPath: "/assets/logos/dankscreen.png"},
+				{Title: "DankTodo", Link: "https://todo.dankstuff.net", Description: "The first htmx app with C (Ulfius)", LogoPath: "/assets/logos/danktodo.png"},
+				{Title: "DankNotes", Link: "https://notes.dankstuff.net", Description: "Bootleg version of Notion and Google Notes.", WIP: true},
+				// {Title: "DankSim", Link: "https://danksim.com", Description: "Get an eSIM qucickly, ANYWHERE.", WIP: true},
+				{Title: "DankDysk", Link: "https://dankdysk.com", Description: "Magical unlimited cloud drive.", WIP: true},
+				{Title: "DankTorrent", Link: "https://dankdysk.com", Description: "Download anything as torrent.", WIP: true},
+				{Title: "DankIP", Link: "https://dankip.com", Description: "Get a public IP for any server/computer.", WIP: true},
+				{Title: "libdank", Link: "https://libdank.org", Description: "File compression extension but the result is a video.", WIP: true},
 			},
 		})
 	})
